7fb615ca6fb38b1a8730d6965f69e34d465398f3
TheWalkingForest <fwalker@tutamail.com>
2026-06-17 11:10:29 -0700

    Cleanup makefile and remaining services

--- blackcat/shepherd/services/core.scm
+++ blackcat/shepherd/services/core.scm
@@ -142,3 +142,34 @@
   (service
    '(system)
    #:requirement '(halt-hook)))
+
+(define-public %core-services
+  '(seedrng
+    hwclock
+    udevadm
+    pkill
+    filesystems
+    halt-hook
+    system
+    agetty-tty1
+    agetty-tty2
+    agetty-tty3
+    agetty-tty4
+    agetty-tty5
+    agetty-tty6))
+
+(define-public %core-services-service
+  (list
+   seedrng-service
+   hwclock-service
+   udevadm-service
+   pkill-service
+   filesystems-service
+   halt-hook-service
+   system-service
+   agetty-tty1-service
+   agetty-tty2-service
+   agetty-tty3-service
+   agetty-tty4-service
+   agetty-tty5-service
+   agetty-tty6-service))
